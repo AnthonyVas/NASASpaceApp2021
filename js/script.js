ded5c0a74e46bf716789ca03a7476c25397990f9
hector martinez <mejia.martinez2012@hotmail.es>
2021-10-03 00:10:54 -0600

    cambios xd

--- js/script.js
+++ js/script.js
@@ -4,32 +4,14 @@ $(document).ready(function () {
         e.preventDefault();
         loadData('SV-SS');
         
-      $.ajax({
-        url: 'https://api.openweathermap.org/data/2.5/weather?q=San Salvador,SV&units=metric&appid=331e87080967381398c662bf2c28050c',
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            $('#texthome').text(data.name);
-            $('#txt').text(data.main.temp + "°C");
-           
-        }
-    });
+
 });
 
     $('#SV-SM').click(function (e) {
         e.preventDefault();
         loadData('SV-SM');
         
-        $.ajax({
-            url: 'https://api.openweathermap.org/data/2.5/weather?q=San Miguel,SV&units=metric&appid=331e87080967381398c662bf2c28050c',
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-                $('#texthome').text(data.name);
-                $('#txt').text(data.main.temp + "°C");
-
-            }
-        });
+        
     });
     $('#SV-UN').click(function (e) {
         e.preventDefault();
