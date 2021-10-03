e86c701b04e3d20a10e965b5ff5a3c34e6be19fd
AnthonyVas <vasquezanth@gmail.com>
2021-10-03 00:27:03 -0600

    merge

--- js/script.js
+++ js/script.js
@@ -3,39 +3,58 @@ $(document).ready(function () {
     $('#SV-SS').click(function (e) {
         e.preventDefault();
         loadData('SV-SS');
-        
+        $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/weather?q=San Salvador,SV&units=metric&appid=331e87080967381398c662bf2c28050c',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                $('#texthome').text(data.name);
+                $('#txt').text(data.main.temp + "°C");
 
-});
+            }
+        });
+
+    });
 
     $('#SV-SM').click(function (e) {
         e.preventDefault();
         loadData('SV-SM');
-        
-        
+        $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/weather?q=San Miguel,SV&units=metric&appid=331e87080967381398c662bf2c28050c',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                $('#texthome').text(data.name);
+                $('#txt').text(data.main.temp + "°C");
+
+            }
+        });
     });
+
+
     $('#SV-UN').click(function (e) {
         e.preventDefault();
         loadData('SV-SM');
-        
+
     });
-    $('#SV-UN').hover(function (e) {
-        e.preventDefault();
-        alert("america");
-        
-    });
+    // $('#SV-UN').hover(function (e) {
+    //     e.preventDefault();
+    //     alert("america");
+
+    // });
 });
 
-function loadData(id){
-    switch (id){
+function loadData(id) {
+    switch (id) {
         case 'SV-SS':
             $('#texthome').text("Hola San Salvador");
-        break;
+            break;
         case 'SV-SM':
             $('#texthome').text("Hola San Miguel");
-        break;
+            break;
         case 'SV-UN':
             $('#texthome').text("Hola La Union");
-        break;
+            break;
         default:
             alert("Sorry we got internal troubles.")
     }
@@ -44,22 +63,22 @@ function loadData(id){
 /*FOR THE TOOLTIP */
 $description = $(".description");
 
-  $('.land').hover(function() {
-    
+$('.land').hover(function () {
+
     $(this).attr("class", "enabled heyo");
     $description.addClass('active');
     $description.html($(this).attr('id'));
-  }, function() {
+}, function () {
     $description.removeClass('active');
-  });
+});
 
-$(document).on('mousemove', function(e){
-  
-  $description.css({
-    left:  e.pageX,
-    top:   e.pageY - 70
-  });
-  
+$(document).on('mousemove', function (e) {
+
+    $description.css({
+        left: e.pageX,
+        top: e.pageY - 70
+    });
+
 });
 
 /*/FOR THE TOOLTIP */
