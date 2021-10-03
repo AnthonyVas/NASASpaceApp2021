712dfb82385a0717aa47759719b3bf840b504eb8
hector martinez <mejia.martinez2012@hotmail.es>
2021-10-02 23:54:31 -0600

    agregando datos de la api

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 $(document).ready(function () {
+
     $('#SV-SS').click(function (e) {
         e.preventDefault();
-        $('#texthome').text("Hola San Salvador");
-        // var t = this.getBoundingClientRect().top,
-        //     l = this.getBoundingClientRect().left;
-        // $tooltip.css({"top": t + "px", "left": l + "px"}).show();
+      $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather?q=San Salvador,SV&units=metric&appid=331e87080967381398c662bf2c28050c',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            $('#texthome').text(data.name);
+            $('#txt').text(data.main.temp + "°C");
+           
+        }
     });
+});
+
     $('#SV-SM').click(function (e) {
         e.preventDefault();
-        $('#texthome').text("Hola San Miguel");
-        // var t = this.getBoundingClientRect().top,
-        //     l = this.getBoundingClientRect().left;
-        // $tooltip.css({"top": t + "px", "left": l + "px"}).show();
+        $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/weather?q=San Miguel,SV&units=metric&appid=331e87080967381398c662bf2c28050c',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                $('#texthome').text(data.name);
+                $('#txt').text(data.main.temp + "°C");
+
+            }
+        });
     });
 });
